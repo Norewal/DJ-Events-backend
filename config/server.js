cce7345c5f33cb3716f5bcabe3a74e56b8266353
Norewal <63234816+Norewal@users.noreply.github.com>
2021-10-16 18:22:30 +0200

    DJ event - Strapi backend

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'd0090f7d8533fc5b37cfbd4455e74891'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'a49c2a97-4521-4093-91a8-083fd11019f8'
+};
